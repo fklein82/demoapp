5060e95a40bf2bc9ba8ab3d9f272fc81cd365677
fklein82 <frederic.klein@gmail.com>
2022-07-27 18:48:24 +0200

    first commit

---
+++ src/main/java/com/tanzu/fkl/FklDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tanzu.fkl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FklDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FklDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanzu/fkl/FklDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanzu.fkl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FklDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
